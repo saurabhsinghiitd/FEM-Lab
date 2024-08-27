cebf6ec2416fbc513adfd0f7f68148e5d0888c49
saurabhsinghiitd <87124869+saurabhsinghiitd@users.noreply.github.com>
2024-08-27 13:37:02 +0530

    Create truss2d.jl

---
+++ truss2d.jl
@@ -0,0 +1,10 @@
+##################################################################
+###########        FEM Lab Course -by Saurabh Singh    ###########
+##################################################################
+
+using LinearAlgebra
+
+function main()
+  # Write a program to solve a general 2D truss problem using FEM
+  
+end
